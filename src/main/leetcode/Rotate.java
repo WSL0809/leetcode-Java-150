52709b4e6c30e1b3fc6aa1835d21bd853618d9e6
WangSL <48207171+WSL0809@users.noreply.github.com>
2023-09-07 14:40:48 +0800

    test

--- src/main/leetcode/Rotate.java
+++ src/main/leetcode/Rotate.java
@@ -1,7 +1,20 @@
 package main.leetcode;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Rotate {
     public static void rotate(int[] nums, int k) {
-
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        Arrays.stream(nums).forEach(queue::add);
+        while (k > 0) {
+            queue.addFirst(queue.pollLast());
+            k--;
+        }
+//        System.out.println(queue);
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]= queue.pollFirst();
+        }
+//        System.out.println(Arrays.toString(nums));
     }
 }
---
+++ src/test/testRotate.java
@@ -0,0 +1,12 @@
+package test;
+
+import main.leetcode.Rotate;
+import org.junit.Test;
+
+public class testRotate {
+    @Test
+    public void test() {
+        int[] nums = {1,2};
+        Rotate.rotate(nums, 3);
+    }
+}
